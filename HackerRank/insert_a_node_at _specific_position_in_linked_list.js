6a2968b2bce625b47e2138994cef088ad2cff977
Zubayer Hossain <zubayer94@gmail.com>
2022-06-22 18:05:35 +0600

    solved Insert a node at a specific position in a linked list

---
+++ HackerRank/insert_a_node_at _specific_position_in_linked_list.js
@@ -0,0 +1,42 @@
+
+
+/*
+ * Complete the 'insertNodeAtPosition' function below.
+ *
+ * The function is expected to return an INTEGER_SINGLY_LINKED_LIST.
+ * The function accepts following parameters:
+ *  1. INTEGER_SINGLY_LINKED_LIST llist
+ *  2. INTEGER data
+ *  3. INTEGER position
+ */
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode next;
+ * }
+ *
+ */
+
+function insertNodeAtPosition(head, data, position) {
+    let count = 0;
+    let prev = null;
+    let current = head;
+    let newNode = new SinglyLinkedListNode(data);
+
+    while (count < position && current) {
+        prev = current;
+        current = current.next;
+        count++;
+    }
+
+    if (prev) {
+        prev.next = newNode;
+        newNode.next = current;
+    }
+
+    return head;
+
+}
