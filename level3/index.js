3b9c8275fd78e069f534fd732de3d49dd4baaa03
Zubayer Hossain <zubayer94@gmail.com>
2022-06-28 22:14:16 +0600

    solved whether two strings are anagram of each other

--- level3/index.js
+++ level3/index.js
@@ -3,31 +3,27 @@
 Listen - Silent 
 Triangle - Integral */
 
-// let str1 = 'bfacdre'
-// let str2 = str1.split('').sort()
-// console.log(str2);
 const areAnagram = (str1, str2) => {
-
-    let s1 = str1.split('')
-    let s2 = str2.split('')
-    if (s1.length != s2.length()) 
+    
+    let s1 = str1.split('').sort()
+    let s2 = str2.split('').sort()
+    if (s1.length != s2.length) 
         return false;
 
     // Compare sorted strings
-    str1.forEach((str, index) => {
-        if(str2[index] != str) 
-            return false;
-    });    
-
+    for (let i = 0; i < s1.length; i++) {
+        if(s1[i] != s2[i])
+        return false;
+    }
     return true;
 }
 
 /* Driver Code*/
 let str1 = 'test';
-let str2 = 'ttew';
+let str2 = 'ttes';
 
 // Function Call
 if (areAnagram(str1, str2))
-    console.log("The two strings are anagram of each other");
+    console.log("These are anagram!");
 else
-    console.log("The two strings are not anagram of each other<br>");
+    console.log("Not anagram!");
