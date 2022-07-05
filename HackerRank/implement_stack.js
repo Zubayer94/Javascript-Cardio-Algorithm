8f32118b35ad130e945d8d9e882087defb6c1ebe
Zubayer Hossain <zubayer94@gmail.com>
2022-07-05 13:52:15 +0600

    added stack

---
+++ HackerRank/implement_stack.js
@@ -0,0 +1,28 @@
+class Stack {
+    constructor() {
+        this.items = []
+    }
+
+    push(element) {
+        return this.items.push(element)
+    }
+
+    pop() {
+        return this.items.pop()
+    }
+
+    printStack() {
+        console.log('------printing------');
+        for (let i = 0; i < this.items.length; i++) {
+            console.log(this.items[i]);
+        }
+    }
+}
+
+const stack = new Stack();
+stack.push(1)
+stack.push(2)
+stack.push(3)
+stack.printStack()
+stack.pop()
+stack.printStack()
