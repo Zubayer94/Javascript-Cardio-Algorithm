6fe0271ce63daaeaad5d929ef5cb38ddbcd15e8e
Zubayer Hossain <zubayer94@gmail.com>
2022-11-25 23:36:29 +0600

    validateip solution

--- level3/index.js
+++ level3/index.js
@@ -19,11 +19,49 @@ const areAnagram = (str1, str2) => {
 }
 
 /* Driver Code*/
-let str1 = 'test';
-let str2 = 'ttes';
+// let str1 = 'test';
+// let str2 = 'ttes';
+
+// // Function Call
+// if (areAnagram(str1, str2))
+//     console.log("These are anagram!");
+// else
+//     console.log("Not anagram!");
+
+/* 
+Program to validate an IP address
+Write a program to Validate an IPv4 Address. 
+According to Wikipedia, IPv4 addresses are canonically represented in dot-decimal notation, 
+which consists of four decimal numbers, each ranging from 0 to 255, 
+separated by dots, e.g., 172.16.254.1 */
+
+function validateIP(ip) {
+    let splitedIP = ip.split('.')
+    if (splitedIP.length > 4 || splitedIP.length < 4) {
+        return false
+    }
+    
+    for (let i = 0; i < splitedIP.length; i++) {
+        if(isNaN(splitedIP[i]))
+            return false
+
+        // if (splitedIP[i]) {
+        //     if (plitedIP[i].length <1) {
+                
+        //     }
+        // }
+
+        if(splitedIP[i]>255 || splitedIP[i]<0)
+            return false
+
+        return true
+    }
+}
 
 // Function Call
-if (areAnagram(str1, str2))
-    console.log("These are anagram!");
-else
-    console.log("Not anagram!");
+// if (validateIP('125.512.100.1'))
+//     console.log("Valid IP");
+// else
+//     console.log("Invalid IP!");
+
+
