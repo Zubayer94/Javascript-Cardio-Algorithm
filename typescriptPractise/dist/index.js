f9d3d942de3f2237263c236fe36bd1e090046a89
Zubayer Hossain <hossain@tikweb.com>
2022-11-25 16:17:22 +0600

    compiled

--- typescriptPractise/dist/index.js
+++ typescriptPractise/dist/index.js
@@ -1,9 +1,7 @@
 "use strict";
+var _a;
 let x = 123456789;
 console.log(x);
-function render(document) {
-    console.log(document);
-}
 let numArray = [1, 2, 3];
 let user = [1, 'zubayer'];
 user.push(1);
@@ -24,13 +22,67 @@ var AnotherSize2;
     AnotherSize2[AnotherSize2["Large"] = 3] = "Large";
 })(AnotherSize2 || (AnotherSize2 = {}));
 ;
-var Size;
-(function (Size) {
-    Size["Small"] = "s";
-    Size["Medium"] = "m";
-    Size["Large"] = "l";
-})(Size || (Size = {}));
 ;
-let mySize = Size.Large;
+let mySize = "l";
 console.log(mySize);
+function calculateTax(income, taxYear = 2022) {
+    if (taxYear < 2022)
+        return income * 1.2;
+    return income * 1.3;
+}
+calculateTax(10000);
+let employee = {
+    id: 1,
+    name: 'zuabyer',
+    retire: (date) => {
+        console.log(date);
+    }
+};
+function render(document) {
+    if (typeof document === 'string') {
+        document.toUpperCase();
+    }
+}
+function reject(message) {
+    throw new Error(message);
+}
+function processEvents() {
+    while (true) {
+    }
+}
+console.log('end of the code');
+function kgToLbs(weight) {
+    if (typeof weight == 'number') {
+        return weight * 2.2;
+    }
+    else {
+        return parseInt(weight) * 1.5;
+    }
+}
+kgToLbs(10);
+kgToLbs('10');
+let textBox = {
+    drag: () => { },
+    resize: () => { }
+};
+let quantity = 100;
+function greet(name) {
+    if (name) {
+        console.log(name.toUpperCase());
+    }
+    else
+        console.log('hola!');
+}
+greet(undefined);
+function getCustomer(id) {
+    return id === 0 ? null : { birthday: new Date() };
+}
+let customer = getCustomer(0);
+console.log((_a = customer === null || customer === void 0 ? void 0 : customer.birthday) === null || _a === void 0 ? void 0 : _a.getFullYear());
+let logFun = null;
+logFun === null || logFun === void 0 ? void 0 : logFun('a');
+let speed = null;
+let ride = {
+    speed: speed !== null && speed !== void 0 ? speed : 30
+};
 //# sourceMappingURL=index.js.map
