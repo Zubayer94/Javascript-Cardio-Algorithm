d546391748026cbdfd2598c124727fac711d33fa
Zubayer Hossain <hossain@tikweb.com>
2022-11-28 12:48:35 +0600

    getters and setters

--- typescriptPractise/src/oop.ts
+++ typescriptPractise/src/oop.ts
@@ -1,29 +1,44 @@
 class Account {
-    // read only property
-    readonly id: number;
-    owner: string;
-    balance: number;
-    // optional property
     nickname?: string;
 
-    constructor(id: number, owner: string, balance: number) {
-        this.id = id;
-        this.owner = owner;
-        this.balance = balance;
+    constructor(
+        public readonly id: number, 
+        public owner: string, 
+        private _balance: number) {
+
     }
 
     deposit(amount: number): void {
         if (amount<= 0) 
             throw new Error("Invalid amount");
         
-        this.balance += amount   
+        this._balance += amount   
     }
+
+    private calculateTax() {
+
+    }
+
+    get balance(): number {
+        return this._balance
+    }
+
+    // set balance(value: number) {
+    //     if(value < 0)
+    //         throw new Error("Invalid value");
+    //     this._balance = value
+    // }
 }
 
 let account = new Account(1, 'Zubayer', 0)
+console.log(account.balance);
+// account.balance = 1
 
 account.deposit(100)
 console.log(typeof account);
 console.log(account instanceof Account);
+
+
+
 
 
