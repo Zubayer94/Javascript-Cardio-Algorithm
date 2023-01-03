bc9c22cb7cf34bbb80c18dca8a4fd746f70636dc
Zubayer Hossain <hossain@tikweb.com>
2023-01-03 14:03:31 +0600

    added generic function

--- typescriptPractise/src/generics.ts
+++ typescriptPractise/src/generics.ts
@@ -13,7 +13,31 @@ we colud solved it using any, but we loose type checking, intellence, properties
 other solution is duplicate the class. But it will increase redundency as development pase increases.
 */
 
-/* Generic Constraints -> common and reuseable solution*/
-function echo<T>(value: T): T {
-    return value
+/* Generic Constraints -> common and reuseable solution */
+/* generic classes */
+/* T is generic type perameters. This idea comes from c++. In c++ this classes are called template classes. Generics classes are same as template classes in c++. T in short is template  */
+class KeyValuePairClass<T, U>{ 
+    constructor(public key: T, public value: U){}
 }
+
+// let pairfinal = new KeyValuePairClass<string, string>('1', 'a')
+let pairfinal = new KeyValuePairClass('1', 'a') // most of the time we don't have to explecitely supply generics, the compiler can infer the type of the value
+// pairfinal.key
+
+/* Generic Function */
+class ArrayUtils {
+    static wrapArray<T>(value: T) {
+        return [value];
+    }
+}
+function wrapInArray<T>(value: T) {
+    return [value]
+}
+
+// let number = wrapInArray(1)
+let number = ArrayUtils.wrapArray(1)
+
+// function echo<T>(value: T): T {
+//     return value
+// }
+
